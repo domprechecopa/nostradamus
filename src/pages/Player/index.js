679cc842c7f8377a232b10ba891aa2e2cc9ceaa7
Pedro Pacheco <domprechecopa@gmail.com>
2020-04-14 23:54:01 -0300

    Fix not find conatiner variable

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PlayerViewStatus from '../../components/PlayerVIewStatus';
+import { Container } from './style';
 
 export default function Player(){
     return (
